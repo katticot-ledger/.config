b6baf4cad4b6afc7ec40cc3abd5c9e339b002525
Keita ATTICOT <keita.atticot@ledger.fr>
2023-11-22 17:25:59 +0100

    feat(fish) : improve ldeploy

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,30 @@
-fish_vi_key_bindings
+# Initialize zoxide for fish shell
 zoxide init fish | source
+
+# Initialize starship prompt for fish shell
 starship init fish | source
-git config --global core.excludesfile .config/.gitignore
+
+# Global Git configuration for excludes file
+git config --global core.excludesfile ~/.config/.gitignore
+
+# Set environment variable for VAULT_WORKSPACE_DIR
 set VAULT_WORKSPACE_DIR /Users/keita.atticot/Code/Ledger/vault
+
+# Extend PATH with Python binaries directory
 set -gx PATH /Users/keita.atticot/Library/Python/3.9/bin $PATH
 
+# Check if OPENAI_API_KEY is already set
+if not set -q OPENAI_API_KEY
+    # Attempt to set OPENAI_API_KEY from 1Password if it's not set
+    set -gx OPENAI_API_KEY (op read op://private/OpenAI/credential --no-newline)
 
+    # Additional check to see if OPENAI_API_KEY was successfully set
+    if not set -q OPENAI_API_KEY
+        echo "Error: OPENAI_API_KEY is not set. Please ensure it is available."
+    end
+end
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+# Prepend Rancher Desktop binaries to PATH
 set --export --prepend PATH "/Users/keita.atticot/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
--- fish/functions/ldeploy.fish
+++ fish/functions/ldeploy.fish
@@ -1,6 +1,6 @@
-function ldeploy
+function ldeploy -a name
     # Execute the first command in the background
-    ledger-vault deploy --name ts-migrate --owner @keita --expirationHours 24 --remoteURL https://remote.minivault.ledger-sbx.com &
+    ledger-vault deploy --name $name --owner @keita --expirationHours 24 --remoteURL https://remote.minivault.ledger-sbx.com &
 
     # Capture the process ID (PID) of the background process
     set pid $last_pid
@@ -9,6 +9,6 @@ function ldeploy
     wait $pid
 
     # Execute the second command after the first one finishes
-    ledger-vault bake --preset beatles --minivaultURL https://ts-migrate.minivault.ledger-sbx.com -s saltqa
-end
+    ledger-vault bake --preset beatles --minivaultURL https://$name.minivault.ledger-sbx.com -s saltqa
 
+end
