f18ba713ea34da94d5daa5ac52da0a3b90ff10ce
Keita ATTICOT <keita.atticot@ledger.fr>
2023-12-15 17:54:37 +0100

    fix(fish): fix ldeploy vault

--- fish/functions/ldeploy.fish
+++ fish/functions/ldeploy.fish
@@ -1,21 +1,3 @@
-function ldeploy -a name
-    if test -z "$name"
-        set name work-in-progess
-    end # Execute the first command in the background
-    nvm use 16
-    ledger-vault deploy --name $name --owner @keita --expirationHours 24 --remoteURL https://remote.minivault.ledger-sbx.com &
-
-    # Capture the process ID (PID) of the background process
-    set pid $last_pid
-
-    # Wait for the first command to complete
-    wait $pid
-
-    # Execute the second command after the first one finishes
-    ledger-vault bake --preset beatles --minivaultURL https://$name.minivault.ledger-sbx.com -s saltqa
-    telepresence connect -n minivault-csvexport-sbx
-end
-
 function ldeploy -a name -a useTelepresence
     if test -z "$name"
         set name work-in-progress
