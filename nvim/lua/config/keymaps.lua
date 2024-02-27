348a594b05c7ce784bcce5b29cc76b8a15ab1959
Keita ATTICOT <keita.atticot@ledger.fr>
2024-02-27 18:32:27 +0100

    chore(nvim) : adapt nvim to obsidian.nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+map("n", "<leader><tab>r", "<cmd>ObsidianQuickSwitch<cr>", { desc = "ObsidianQuickSwitch" })
+map("n", "<leader><tab>t", "<cmd>ObsidianToday<cr>", { desc = "ObsidianToday" })
+map("n", "<leader><tab>n", "<cmd>ObsidianNew<cr>", { desc = "ObsidianNew" })
+map("n", "<leader><tab>h", "<cmd>put =strftime('%c')<CR>", { desc = "Insert current date and time" })
