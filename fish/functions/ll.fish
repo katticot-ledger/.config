57940169eeb96ecd9273a889b2511af97e30a148
Keita ATTICOT <keita.atticot@ledger.fr>
2024-11-03 20:13:34 +0100

    fix(eza): fix ll command

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,4 +1,5 @@
 function ll --wraps=ls
     # Display files in list view with sizes, icons, headers, and hidden files
-    eza --long --header --all --no-user --no-permissions --icons --group-directories-first --color=always --binary
+    # Use $argv to specify a directory, or default to the current directory if no argument is given
+    eza --long --header --all --no-user --no-permissions --icons --group-directories-first --color=always --binary $argv
 end
