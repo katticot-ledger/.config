ee536017803e62b666ac270ef0a97e705c080046
Keita ATTICOT <keita.atticot@ledger.fr>
2024-11-05 17:20:22 +0100

    feat(nvim): add themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -782,16 +782,16 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-
+  { 'EdenEast/nightfox.nvim' },
+  { 'navarasu/onedark.nvim' },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
