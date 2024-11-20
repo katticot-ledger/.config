b478b4c0d8492452340383e80ec7fcb11579ebeb
Keita ATTICOT <keita.atticot@ledger.fr>
2024-11-20 20:08:00 +0100

    fix(fish): fix obsidian vault

--- fish/functions/fobs.fish
+++ fish/functions/fobs.fish
@@ -1,6 +1,7 @@
 function fobs
-    set selected_file (fd --exclude raycast --type f   . '/Users/keita.atticot/Google Drive/My Drive/obsidian' | fzf --preview "bat --style=numbers --color=always --line-range=:500 {}")
-    if test -n "$selected_file"
-        nvim "$selected_file"
+    set base_dir '/Users/keita.atticot/Library/CloudStorage/GoogleDrive-keita.atticot@ledger.fr/My Drive/obsidian'
+    set selected_file (fd --exclude raycast --type f . $base_dir | sed "s|$base_dir/||" | fzf --preview "bat --style=numbers --color=always --line-range=:500 $base_dir/{}")
+    if test $selected_file
+        nvim "$base_dir/$selected_file"
     end
 end
