db276e30ca252eda0aeb720117ea0ec638ffea06
Keita ATTICOT <keita.atticot@ledger.fr>
2024-12-26 17:05:49 +0100

    fix obsidian file listing

--- fish/functions/fobs.fish
+++ fish/functions/fobs.fish
@@ -1,7 +1,15 @@
 function fobs
-    set base_dir '/Users/keita.atticot/Library/CloudStorage/GoogleDrive-keita.atticot@ledger.fr/My Drive/obsidian'
-    set selected_file (fd --exclude raycast --type f . $base_dir | sed "s|$base_dir/||" | fzf --preview "bat --style=numbers --color=always --line-range=:500 $base_dir/{}")
+    set base_dir '/Users/keita.atticot/Library/CloudStorage/GoogleDrive-keita.atticot@ledger.fr/My\ Drive/obsidian'
+
+    cd $base_dir
+    set selected_file (fd --exclude raycast --type f . | \
+       fzf --preview "bat --style=numbers --color=always --line-range=:500 {}" \
+       --height 40% \
+       --border rounded \
+       --preview-window right:60% \
+       --bind 'ctrl-/:toggle-preview')
+
     if test $selected_file
-        nvim "$base_dir/$selected_file"
+        nvim "$selected_file"
     end
 end
