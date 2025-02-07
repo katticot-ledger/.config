dc366e7cf10a0cf13ed38a68d7e6303db97241f2
Keita ATTICOT <keita.atticot@ledger.fr>
2025-02-07 16:58:59 +0100

    chore(nvim): cleaning the config

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,52 +73,89 @@ vim.api.nvim_set_hl(0, 'CursorLine', { ctermbg = 0, bg = '#3b82f6' })
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 20
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
---
--- Quit Neovim
-vim.keymap.set('n', '<leader>aa', '<cmd>qa<CR>', { desc = 'Quit Neovim' })
 
--- Save
+-- ==================================================
+-- 🔑 [[ BASIC KEYMAPS ]]
+-- ==================================================
+
+-- ✂️ Remap 'x' to avoid yanking in normal and visual mode
+vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'x', '"_x', { noremap = true, silent = true })
+
+-- ==================================================
+-- 💾 SAVE & QUIT
+-- ==================================================
+
+-- Save and quit using 'AA' in normal mode
+vim.keymap.set('n', 'AA', ':wq<CR>', { noremap = true, silent = true })
+
+-- Save file with Ctrl+S
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save' })
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- ==================================================
+-- 🔍 SEARCH & HIGHLIGHTING
+-- ==================================================
 
--- Diagnostic keymaps
+-- Clear search highlights on pressing Esc
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
+
+-- ==================================================
+-- 🛠️ DIAGNOSTICS
+-- ==================================================
+
+-- Open diagnostic quickfix list
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Open Mini files navigator
-vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>', { desc = 'Open Mini.files', noremap = true, silent = true })
+-- ==================================================
+-- 📂 FILE NAVIGATION
+-- ==================================================
+
+-- Open Mini Files navigator
+vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>', { desc = 'Open Mini Files', noremap = true, silent = true })
 vim.keymap.set('n', '<CR>', function()
   require('mini.files').synchronize()
   require('mini.files').go_in()
 end, { desc = 'Enter directory and auto-save' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- ==================================================
+-- 📋 COPY FILE PATH
+-- ==================================================
+
+-- Copy relative file path to clipboard
+vim.api.nvim_create_user_command('CopyRelativeFilePath', function()
+  local file_path = vim.fn.expand '%:.'
+  if file_path ~= '' then
+    vim.fn.setreg('+', file_path)
+    print('📋 Relative file path copied to clipboard: ' .. file_path)
+  else
+    print '⚠️ No file is currently open.'
+  end
+end, {})
+vim.keymap.set('n', '<leader>cp', ':CopyRelativeFilePath<CR>', { desc = 'Copy relative file path to clipboard' })
+
+-- ==================================================
+-- 🖥️ TERMINAL MODE
+-- ==================================================
+
+-- Exit terminal mode with Esc Esc
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- ==================================================
+-- 🔀 WINDOW NAVIGATION
+-- ==================================================
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- Window navigation with Ctrl + hjkl
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- ==================================================
+-- ❌ BUFFER MANAGEMENT
+-- ==================================================
+
+-- Close current buffer with <leader>bd
+vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = 'Close current buffer' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -456,7 +493,9 @@ require('lazy').setup({
   { import = 'custom.plugins.flash' },
   { import = 'custom.plugins.noice' },
   { import = 'custom.plugins.files' },
+  { import = 'custom.plugins.lazygit' },
   { import = 'custom.plugins.obsidian' },
+  { import = 'custom.plugins.ai.avante' },
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
