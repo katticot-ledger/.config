211c09c17ae606892e24d69f874c693ece23d7e3
Keita Atticot <sagara911@gmail.com>
2026-01-18 10:11:52 +0100

    feat(nvim): add OpenCode plugin integration and update IDE configuration

--- nvim/lua/custom/plugins/ide.lua
+++ nvim/lua/custom/plugins/ide.lua
@@ -219,6 +219,8 @@ return {
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = vim.tbl_keys(servers),
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
